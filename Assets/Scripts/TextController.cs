e9f2fc6d88596fe7a59299be9f80384ccd5031dd
mbiondi17 <mvb5bg@virginia.edu>
2016-09-19 23:15:18 -0400

    added one new state, committing for workflow ease.

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class TextController : MonoBehaviour {
 
     public Text text;
-    private enum States { caveMouth, WebWall_0, DarkTunnel, DimlyLitTunnel}
+    private enum States { caveMouth, WebWall_0, DarkTunnel, DimlyLitTunnel, WebWall_investigate}
     private States myState;
 
 	// Use this for initialization
@@ -22,7 +22,11 @@ public class TextController : MonoBehaviour {
         }
         else if (myState == States.WebWall_0)
         {
-            state_WebWall_0;
+            state_WebWall_0();
+        }
+        else if (myState == States.WebWall_investigate)
+        {
+            state_WebWall_investigate();
         }
     }
 
@@ -44,6 +48,29 @@ public class TextController : MonoBehaviour {
     {
 
         text.text = "You huff a deep breath and head down the left path. You don't get very far before the hand you use for guidance " +
-                    "comes in contact with something thick and sticky. "
+                    "comes in contact with something thick and sticky. A spider web. Wonderful. You pull back your hand slowly and " + 
+                    "dislodge it. \n\n" + 
+                    
+                    "I to Investigate the web, R to Return to the cave's mouth";
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            myState = States.WebWall_investigate;
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            myState = States.caveMouth;
+        }
+    }
+
+    void state_WebWall_investigate()
+    {
+        text.text = "It's too dark to see how far the web stretches. However, the air here is dry, and you know that spider web melts " +
+                    "on contact with fire. If only you had a torch or something...\n\n" +
+
+                    "R to Return to the cave's mouth";
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            myState = States.caveMouth;
+        }
     }
 }
